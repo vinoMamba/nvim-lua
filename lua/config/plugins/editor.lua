69f3480b9cf4b0fdcb381277ebd94b3f41d137b3
wangxin <vino0908@outlook.com>
2023-07-10 11:54:55 +0800

    添加当前单词高亮

--- lua/config/plugins/editor.lua
+++ lua/config/plugins/editor.lua
@@ -6,17 +6,6 @@ return {
 			require("nvim-autopairs").setup()
 		end
 	},
-	-- 折叠代码
-	-- {
-	-- 	"kevinhwang91/nvim-ufo",
-	-- 	dependencies = { "kevinhwang91/promise-async" },
-	-- 	config = function()
-	-- 		require('ufo').setup()
-	-- 		vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-	-- 		vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-	-- 	end	
-	-- },
-
 	{ 'gcmt/wildfire.vim',  lazy = false, }, -- 按回车选中
 	{ 'tpope/vim-surround', lazy = false },
 	-- 颜色显示
@@ -46,17 +35,18 @@ return {
 			buftypes = {},
 		}
 	},
-	-- {
-	-- 	"RRethy/vim-illuminate",
-	-- 	config = function()
-	-- 		require('illuminate').configure({
-	-- 			providers = {
-	-- 				-- 'lsp',
-	-- 				-- 'treesitter',
-	-- 				'regex',
-	-- 			},
-	-- 			under_cursor = true
-	-- 		})
-	-- 	end
-	-- },
+	-- 高亮当前单词
+	{
+		"RRethy/vim-illuminate",
+		config = function()
+			require('illuminate').configure({
+				providers = {
+					-- 'lsp',
+					-- 'treesitter',
+					'regex',
+				},
+				under_cursor = true
+			})
+		end
+	},
 }
