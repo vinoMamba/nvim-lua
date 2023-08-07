32a77af51c14ab376dd579b43cede305b0a1b945
vino <vino0908@outlook.com>
2023-08-08 00:01:38 +0800

    indent line

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -83,5 +83,30 @@ return {
         enabled = false,
       },
     }
-  }
+  },
+  -- indent blankline
+  {
+		"shellRaining/hlchunk.nvim",
+		init = function()
+			require('hlchunk').setup({
+				chunk = {
+					enable = true,
+					use_treesitter =true,
+					style = {
+						{ fg = "#806d9c" },
+					},
+				},
+				indent = {
+					chars = { "│", "¦", "┆", "┊", },
+					use_treesitter = false,
+				},
+				blank = {
+					enable = false,
+				},
+				line_num = {
+					use_treesitter = true,
+				},
+			})
+		end
+	}, 
 }
