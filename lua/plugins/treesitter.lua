e4b0fba00e2a3a1bbe908418a2455f4cf2c17b2c
wangxin <vino0908@outlook.com>
2023-08-08 13:10:49 +0800

    treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,18 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup{
+        ensure_installed = "all",
+				highlight = {
+					enable = true,
+					disable = {}, -- list of language that will be disabled
+				},
+				indent = {
+	        enable = true
+				},
+      }
+    end
   }
 }
