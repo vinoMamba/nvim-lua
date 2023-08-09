acb827d16df1134a844b20c9104b3f7f2daea4a2
vino <vino0908@outlook.com>
2023-08-09 23:32:55 +0800

    go lang

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -73,6 +73,7 @@ return {
         },
         servers = {
           ['lua_ls'] = { 'lua' },
+          ['gopls'] = { 'go' },
           -- if you have a working setup with null-ls
           -- you can specify filetypes it can format.
           -- ['null-ls'] = {'javascript', 'typescript'},
