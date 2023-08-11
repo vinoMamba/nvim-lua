72c15dd6d6b61b28fc0ce0341c2c7b2294e8b94f
wangxin <vino0908@outlook.com>
2023-08-11 11:25:03 +0800

    update indent plugin config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -96,18 +96,31 @@ return {
           enable = true,
           use_treesitter = true,
           style = {
-            { fg = "#806d9c" },
+            { fg = "#87afaf" },
           },
         },
         indent = {
+          enable = false,
           chars = { "│", "¦", "┆", "┊", },
           use_treesitter = false,
+          style = {
+            "#FF0000",
+            "#FF7F00",
+            "#FFFF00",
+            "#00FF00",
+            "#00FFFF",
+            "#0000FF",
+            "#8B00FF",
+          },
         },
         blank = {
           enable = false,
         },
         line_num = {
           use_treesitter = true,
+          style = {
+            { fg = "#87afaf" },
+          },
         },
       })
     end
