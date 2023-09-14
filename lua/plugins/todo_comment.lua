b75b7127bc83f0cfe947bf14b151e6e0de76ada1
vino <vino0908@outlook.com>
2023-09-14 10:25:43 +0800

    refactor tool plugin

---
+++ lua/plugins/todo_comment.lua
@@ -0,0 +1,14 @@
+  -- TODO: todo
+  -- HACK:hack
+  -- WARN: warn
+  -- PERF: perf
+  -- NOTE: note
+  -- TEST: test
+return  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true,
+    keys = {
+      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+    },
+  }
