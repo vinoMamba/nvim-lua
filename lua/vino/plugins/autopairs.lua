bf118627bdc1ff01eaf8e3bf5819941a02565f6a
vino <vino0908@outlook.com>
2023-09-16 00:47:35 +0800

    gurad fmt plugin

--- lua/vino/plugins/autopairs.lua
+++ lua/vino/plugins/autopairs.lua
@@ -1,5 +1,5 @@
-return  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {}
-  }
+return {
+  'windwp/nvim-autopairs',
+  event = "InsertEnter",
+  opts = {}
+}
---
+++ lua/vino/plugins/lsp/formatter.lua
@@ -0,0 +1,22 @@
+return {
+  "nvimdev/guard.nvim",
+  -- Builtin configuration, optional
+  dependencies = {
+    "nvimdev/guard-collection",
+  },
+  config = function()
+    local ft = require('guard.filetype')
+
+    ft('lua'):fmt('lsp'):append('stylua')
+
+    -- Call setup() LAST!
+    require('guard').setup({
+      -- the only options for the setup function
+      fmt_on_save = false,
+      -- Use lsp if no formatter was defined for this filetype
+      lsp_as_default_formatter = false,
+    })
+
+    vim.keymap.set({ 'n', }, '<leader>l', '<cmd>GuardFmt<cr>', { silent = true, noremap = true })
+  end
+}
