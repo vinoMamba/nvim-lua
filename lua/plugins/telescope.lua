b95791ce85379738c83d56b7432d22843dd6291b
wangxin <vino0908@outlook.com>
2024-05-07 18:12:55 +0800

    todo comment

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -40,34 +40,25 @@ return {
       vim.keymap.set('n', '<leader>sc',       builtin.commands, { desc = 'Search commands' })
       vim.keymap.set('n', '<leader>sr',       builtin.resume, { desc = 'Search resume' })
       vim.keymap.set('n', '<leader>sd',       builtin.diagnostics, { desc = 'Sdarch diagnostics' })
+      vim.keymap.set('n', '<leader>st',       "<cmd>TodoTelescope<cr>", { desc = 'Sdarch todo comments' })
 
       vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Search files in current dir' })
       vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "telescope live grep" })
-      vim.keymap.set("n", "<leader>;", builtin.current_buffer_fuzzy_find, { desc = "telescope find in current buffer" })
-
-
-      -- Slightly advanced example of overriding default behavior and theme
-      -- vim.keymap.set('n', '<leader>/', function()
-      --   -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      --   builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-      --     winblend = 10,
-      --     previewer = false,
-      --   })
-      -- end, { desc = '[/] Fuzzily search in current buffer' })
+      vim.keymap.set('n', '<leader>;', builtin.current_buffer_fuzzy_find,{ desc = '[/] Fuzzily search in current buffer' })
 
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        }
-      end, { desc = '[S]earch [/] in Open Files' })
-
-      -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
+      -- vim.keymap.set('n', '<leader>s/', function()
+      --   builtin.live_grep {
+      --     grep_open_files = true,
+      --     prompt_title = 'Live Grep in Open Files',
+      --   }
+      -- end, { desc = '[S]earch [/] in Open Files' })
+      --
+      -- -- Shortcut for searching your Neovim configuration files
+      -- vim.keymap.set('n', '<leader>sn', function()
+      --   builtin.find_files { cwd = vim.fn.stdpath 'config' }
+      -- end, { desc = '[S]earch [N]eovim files' })
     end
   }
 }
---
+++ lua/plugins/todo_comment.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    --TODO: todo
+    --FIX: fix
+    --NOTE: note
+    'folke/todo-comments.nvim', 
+    event = 'VimEnter', 
+    dependencies = { 'nvim-lua/plenary.nvim' }, 
+    opts = {},
+  }
+}
