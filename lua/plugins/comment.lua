a9a3c7864406dd4d591dbb110cc90390886b84d6
vino <vino0908@outlook.com>
2024-05-08 21:15:24 +0800

    lazyvim start

--- lua/plugins/comment.lua
+++
@@ -1,4 +0,0 @@
-return {
-  -- Use `opts = {}` to force a plugin to be loaded.
-  { 'numToStr/Comment.nvim', opts = {} },
-}
--- lua/plugins/vim-maximizer.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "szw/vim-maximizer",
-   keys = {
-    { "<leader>f", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize a split" },
-  },
-}
