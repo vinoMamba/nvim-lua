a8f900235388bc4ccbd6e018d2be16b554a42ba8
vino <vino0908@outlook.com>
2024-07-15 17:14:51 +0800

    add golang plugin

--- lua/options.lua
+++ lua/options.lua
@@ -5,5 +5,5 @@ require "nvchad.options"
 local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
+o.scrolloff = 10
 o.wrap = false
-
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,24 +31,34 @@ return {
       require("nvim-tree").setup(opts)
     end,
   },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "airblade/vim-rooter",
+    lazy = false,
+    init = function()
+      vim.g.rooter_patterns = { '__vim_project_root', '.git/' }
+      vim.g.rooter_silent_chdir = true
+      -- set an autocmd
+      vim.api.nvim_create_autocmd("VimEnter", {
+        pattern = "*",
+        callback = function()
+          -- source .vim.lua at project root
+          vim.cmd([[silent! source .vim.lua]])
+        end,
+      })
+    end
+  },
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("configs.golang")
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  }
 }
