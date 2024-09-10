27ed5432e8f50eadf9aaf7ed3928c4826d7954fc
vino <vino0908@outlook.com>
2024-09-10 11:32:13 +0800

    cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,7 +32,7 @@ return {
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     -- INFO: broadcast that to the servers
 
-    -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
     require('mason').setup()
 
