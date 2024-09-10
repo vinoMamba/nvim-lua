189d277887ab0f2114ddab0979a70af6d05b5b7c
vino <vino0908@outlook.com>
2024-09-10 10:42:35 +0800

    lsp

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,20 @@
 return {
-  'nvim-telescope/telescope.nvim', 
-  tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = {
-    defaults = {
-      layout_config = {
-        vertical = { width = 0.5 }
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-ui-select.nvim'
+    },
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+          }
+        }
       }
-    }
-  }
+      require("telescope").load_extension("ui-select")
+    end
+  },
 }
